84fc08981c4cd474352b596500f26b5da132c285
Ashish Kumar <143773138+Ashish-Kumar-45631@users.noreply.github.com>
2024-08-22 12:32:52 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,23 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvXcmjqkNfMS0UGvNrjDSK9P-IFUpe5vs",
-  authDomain: "photofolio-23d04.firebaseapp.com",
-  projectId: "photofolio-23d04",
-  storageBucket: "photofolio-23d04.appspot.com",
-  messagingSenderId: "358983786647",
-  appId: "1:358983786647:web:2a119d209bb220782489d2",
-  measurementId: "G-XD69QY5C3R",
+  apiKey: "AIzaSyBOCrewi5MJ_il4lmaKT0qfd1uqXtY_UnE",
+  authDomain: "photofolio-c46dc.firebaseapp.com",
+  projectId: "photofolio-c46dc",
+  storageBucket: "photofolio-c46dc.appspot.com",
+  messagingSenderId: "646921875489",
+  appId: "1:646921875489:web:96f16f64f828900ef4b360",
+  measurementId: "G-PCQWGHJ5G9"
 };
 
+
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db };
+
+
+
